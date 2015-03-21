765bbf2e06f58194690e1ffdb84cfe5218cd3e11
Jeff Rowe <jeff@saagaradesigns.com>
2015-03-21 01:22:23 -0700

    first commit

---
+++ 2FingerPong/Assets/Scripts/ball.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+	public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+		//init velocity
+		GetComponent<Rigidbody>().velocity = Vector2.right * speed;
+	}
+
+	void OnCollisionEnter(Collision col){
+		//Note: 'col' holds the collision information.
+		//If the ball collided with the racket, then:
+		//	col.gameObect is the racket
+		//	col.transform.position is the racket's position
+		//	col.collider is the racket's collider
+
+		//hit the left racket?
+		if(col.gameObject.name == "RacketLeft"){
+			//calculate hit factor
+			float y = hitFactor(transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+
+			//calculate direction, make length =1 via normalized
+			Vector2 dir = new Vector2(1,y).normalized;
+
+			//set velocity with dir * speed
+			GetComponent<Rigidbody>().velocity = dir * speed;
+		}
+		/*
+		//hit the right racket?
+		if(col.gameObject.name == "RacketRight"){
+			//calculate hit factor
+			float y = hitFactor(transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+
+			//calculate direciton, make length =1 via normalized
+			Vector2 dir = new Vector2(-1,y).normalized;
+
+			//set velocity with dir * speed
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}*/
+
+
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight){
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+}
---
+++ 2FingerPong/Assets/Scripts/clampObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+public class clampObject : MonoBehaviour
+{	
+	void Update ()
+	{
+		if(gameObject.name == "RacketLeft"){
+		var pos = gameObject.transform.localPosition;
+		pos.x =  Mathf.Clamp(transform.localPosition.x, -20f, -20f);
+		pos.y =  Mathf.Clamp(transform.localPosition.y, -12.5f, 12.5f);
+		gameObject.transform.localPosition = pos;
+		}
+		else if(gameObject.name == "RacketRight"){
+			var pos = gameObject.transform.localPosition;
+			pos.x =  Mathf.Clamp(transform.localPosition.x, 20f, 20f);
+			pos.y =  Mathf.Clamp(transform.localPosition.y, -12.5f, 12.5f);
+			gameObject.transform.localPosition = pos;
+		}
+	}
+}
